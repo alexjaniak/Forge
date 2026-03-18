98dd7832ccd6348c2e24121ecabee94fe37cb5d9
alexjaniak <alexander.janiak.acc@outlook.com>
2026-03-18 18:08:16 -0400

    Fix forge-webhook duplicate deprecation warning

--- apps/webhook-monitor/src/forge_webhook/main.py
+++ apps/webhook-monitor/src/forge_webhook/main.py
@@ -3,6 +3,7 @@ import sys
 
 from forge_cli.webhook_server.main import app
 
+
 def run():
     from forge_cli.webhook_server.main import run as canonical_run
 
@@ -12,4 +13,12 @@ def run():
             file=sys.stderr,
         )
 
-    canonical_run()
+    previous_invocation_marker = os.environ.get("_FORGE_WH_INVOKED")
+    os.environ["_FORGE_WH_INVOKED"] = "1"
+    try:
+        canonical_run()
+    finally:
+        if previous_invocation_marker is None:
+            os.environ.pop("_FORGE_WH_INVOKED", None)
+        else:
+            os.environ["_FORGE_WH_INVOKED"] = previous_invocation_marker
